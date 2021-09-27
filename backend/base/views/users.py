5090c4615c8a31ccab222379f979a19df5d7326d
Martin Jasan <martin.jasan@awesomedevelopers.eu>
2021-09-27 15:39:53 +0200

    complete ordering flow

--- backend/base/views/users.py
+++ backend/base/views/users.py
@@ -77,3 +77,8 @@ def get_all_users(request):
     users = User.objects.all()
     serializer = serializers.UserSerializer(users, many=True)
     return Response(serializer.data)
+
+
+def fibonacci(n):
+    if n in {0,1}: return n
+    return fibonacci(n-1) + fibonacci(n-2)
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,11 +1,15 @@
 import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
-    CART_SAVE_SHIPPING_ADDRESS
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD,
 } from "../constants/cartConstants";
 
 
-export const cartReducer = (state = {cartItems: [], shippingAddress: {}}, action) => {
+export const cartReducer = (state = {
+    cartItems: [],
+    shippingAddress: {}
+}, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
@@ -33,6 +37,12 @@ export const cartReducer = (state = {cartItems: [], shippingAddress: {}}, action
                 ...state,
                 shippingAddress: action.payload
             }
+        case     CART_SAVE_PAYMENT_METHOD:
+            return {
+                ...state,
+                paymentMethod: action.payload
+            }
+
 
         default:
             return state
